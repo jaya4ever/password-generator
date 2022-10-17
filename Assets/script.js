d1bdc5d714cd951884cd326ab377dffd82763f67
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-10-16 19:51:47 -0500

    checking password is a string and not empty

--- Assets/script.js
+++ Assets/script.js
@@ -4,83 +4,97 @@ var generateBtn = document.querySelector("#generate");
 window.onload = alert("Please click generate password to start");
 
 // function to generate password and defining variables
-function generatePassword(){
-var upperCaseP = "ABCDEFGHIJKLMNOPQUSTUVWXYZ";
-var lowerCaseP= "abcdefghijklmnopqustuvwxyz";
-var numbersP = "012345678";
-var specialCharP= ":'`~<,>.?/| $%^&*()_-+= ! @ # {}[]";
-var passwordCriteria = "";
+function generatePassword() {
+  var upperCaseP = "ABCDEFGHIJKLMNOPQUSTUVWXYZ";
+  var lowerCaseP = "abcdefghijklmnopqustuvwxyz";
+  var numbersP = "012345678";
+  var specialCharP = ":'`~<,>.?/| $%^&*()_-+= ! @ # {}[]";
+  var passwordCriteria = "";
 
-// validating the input
+  // validating the input
 
-var numberOfCharacters = prompt("How many characters do you want in your password? Please Choose between 8 to 128 characters.");
-  
-// check we have correct number of characters
-if (!(numberOfCharacters.length < 4 && numberOfCharacters.length > 0)) {
-  alert("Password length must be a number between 8-128 characters. Please try again.");
- return(undefined);    
-}
+  var numberOfCharacters = prompt("How many characters do you want in your password? Please Choose between 8 to 128 characters.");
 
-// convert to a integer 
-numberOfCharacters = Number(numberOfCharacters);
+  // check we have correct number of characters
+  if (!(numberOfCharacters.length < 4 && numberOfCharacters.length > 0)) {
+    alert("Password length must be a number between 8-128 characters. Please try again.");
+    return (undefined);
+  }
 
-if (!(numberOfCharacters > 8 && numberOfCharacters <128)){
-  alert("Please enter a value betwee 8 and 128 for the number of characters in the password");
-  return(undefined);
-}
+  // convert to a integer 
+  numberOfCharacters = Number(numberOfCharacters);
 
-//defining lowercase variables to use in password
-var lowerCaseForPassword = confirm("Your password will have lowerCase");
+  if (!(numberOfCharacters > 8 && numberOfCharacters < 128)) {
+    alert("Please enter a value betwee 8 and 128 for the number of characters in the password");
+    return (undefined);
+  }
 
-//defining uppercase variables to use in password
-var upperCaseForPassword = confirm("Your Password will have upperCase");
+  //defining lowercase variables to use in password
+  var lowerCaseForPassword = confirm("Your password will have lowerCase");
 
-//defining number variable to use in password
-var numbersForPassword = confirm("Your password will have numbers");
+  //defining uppercase variables to use in password
+  var upperCaseForPassword = confirm("Your Password will have upperCase");
 
-//defining special character varible to use in password
-var specialCharForPassword = confirm("Your password will have special characters");
+  //defining number variable to use in password
+  var numbersForPassword = confirm("Your password will have numbers");
 
-//using if statement to alert to use the true condition
-if (lowerCaseForPassword == false && upperCaseForPassword ==false && numbersForPassword == false && specialCharForPassword == false) {
-  alert("Please select at least one character type.");
-    return(undefined);
-};
+  //defining special character varible to use in password
+  var specialCharForPassword = confirm("Your password will have special characters");
+
+  //using if statement to alert to use the true condition
+  if (lowerCaseForPassword == false && upperCaseForPassword == false && numbersForPassword == false && specialCharForPassword == false) {
+    alert("Please select at least one character type.");
+    return (undefined);
+  };
 
 
-// including all variables to use it in the password
-if (lowerCaseForPassword) {
-  passwordCriteria += lowerCaseP;
-}
+  // including all variables to use it in the password
+  if (lowerCaseForPassword) {
+    passwordCriteria += lowerCaseP;
+  }
 
-if (upperCaseForPassword) {
-  passwordCriteria += upperCaseP;
-}
+  if (upperCaseForPassword) {
+    passwordCriteria += upperCaseP;
+  }
 
-if (numbersForPassword) {
-  passwordCriteria += numbersP;
-}
+  if (numbersForPassword) {
+    passwordCriteria += numbersP;
+  }
 
-if (specialCharForPassword) {
-  passwordCriteria += specialCharP;
-}
+  if (specialCharForPassword) {
+    passwordCriteria += specialCharP;
+  }
 
-//else {
-//  possibleCharacters += lowercaseCharacters + uppercaseCharacters + numberOfCharacters + specialCharacters;
+  //else {
+  //  possibleCharacters += lowercaseCharacters + uppercaseCharacters + numberOfCharacters + specialCharacters;
 
-alert(passwordCriteria);
+  alert(passwordCriteria);
 
-// picking random numbers for the password
+  // picking random numbers for the password
   var result = "";
   for (let i = 0; i < numberOfCharacters; i++) {
-    var randomForPassword =[Math.floor(Math.random() * passwordCriteria.length)];
+    var randomForPassword = [Math.floor(Math.random() * passwordCriteria.length)];
 
-    result +=passwordCriteria.charAt(randomForPassword);
+    result += passwordCriteria.charAt(randomForPassword);
 
-}
-return result;
+  }
+  return result;
 
 };
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+
+  // Check that password is a string and is not empty. Size validation is in the function generatePassword
+  if (typeof password === 'string' && (password.length > 0)) {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
